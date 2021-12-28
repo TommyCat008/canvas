a514aa4ed9cbbc0418c93c5af648e5bb42f3c670
zhangyaqi <zhangyaqi04@baidu.com>
2021-12-28 19:24:38 +0800

    代码提交

--- dist/text.js
+++ dist/text.js
@@ -1,18 +1,2 @@
-import { Create } from './main.js';
-const canvas = new Create('canvas', {
-    background: '#fff'
-});
-const ctx = canvas.ctx;
-function createText() {
-    canvas.drawText('财', 400, 400, {
-        fontSize: '400px',
-        color: 'gold'
-    });
-}
-function createAntLine() {
-    canvas.clearRect(0, 0, 800, 800);
-    createText();
-    requestAnimationFrame(createAntLine);
-}
-requestAnimationFrame(createAntLine);
+"use strict";
 //# sourceMappingURL=text.js.map
